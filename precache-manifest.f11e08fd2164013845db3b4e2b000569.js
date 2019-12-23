3042f0c913b9689eaf0225a6146e0b5a1619d747
Zhangxia <Xiaohui.Zhang@unisys.com>
2019-12-23 14:42:00 +0800

    Updates

---
+++ precache-manifest.f11e08fd2164013845db3b4e2b000569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11976aa12bae1f80ab47422132e2fc73",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db1d9d1f828c98ac327c",
+    "url": "./static/css/2.3f3fbd6a.chunk.css"
+  },
+  {
+    "revision": "3b85e5ed085b5ca2b074",
+    "url": "./static/css/main.8657aa10.chunk.css"
+  },
+  {
+    "revision": "db1d9d1f828c98ac327c",
+    "url": "./static/js/2.ddfe9ae8.chunk.js"
+  },
+  {
+    "revision": "3b85e5ed085b5ca2b074",
+    "url": "./static/js/main.06f688f7.chunk.js"
+  },
+  {
+    "revision": "4f91b605d0291ff6deb7",
+    "url": "./static/js/runtime-main.c7a13c86.js"
+  }
+]);
